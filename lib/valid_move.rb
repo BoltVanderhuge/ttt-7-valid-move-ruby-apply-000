10131a9968ef4c3e669c68fa7a2c0fc640d33135
John Wisneski <john.j.wisneski@gmail.com>
2020-10-27 03:24:13 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -24,20 +24,8 @@ end
 
  
 
-def position_taken?(board, position)
-
- 
-
-if board[position] == "” ” "|| board[position] == “” || board[position] == nil
-
-false
-
-else board[position] == “X” || board[position] == “O”
-
-true
-
-end
-
- 
-
+def position_taken? (board,index)
+  
+  return !(board[index] == " " || board[index] == "" || board[index] == nil) 
+    
 end
